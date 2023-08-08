c5f1833f23c6b92b02c5fe887d89c30731988ccf
JulioCSimoesP <juliocsimoesp@gmail.com>
2023-08-08 12:25:56 -0300

    Interface UserRepository.php implementada

--- src/Model/Domain/Repository/UserRepository.php
+++ src/Model/Domain/Repository/UserRepository.php
@@ -2,7 +2,11 @@
 
 namespace juliocsimoesp\PHPMVC1\Model\Domain\Repository;
 
+use juliocsimoesp\PHPMVC1\Model\Domain\Entity\User;
+
 interface UserRepository
 {
+    public function createUser(User $user): bool;
 
+    public function userByEmail(string $email): User;
 }
